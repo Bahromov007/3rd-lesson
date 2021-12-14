3d79e6d34cb74eea366fb7b67c342e0b00f71901
Bahromov Abdulhakim <aa0064473@gmail.com>
2021-12-14 16:12:19 +0500

    added 3rd classwork

---
+++ index.py
@@ -0,0 +1,50 @@
+# age=int(input('Enter your age: '))
+# print(type(age))
+# print(age < 18)
+# if age >= 18:
+#     print('welcome')
+# else:
+#     print('Bye')
+
+# num1 = int(input('give the 1st number: '))
+# num2 = int(input('give the 2nd number: '))
+# if num1 > num2:
+#     print(num1)
+# elif num1 ==  num2:
+#     print('the are equal')
+# else:
+#     print(num2)
+
+# login = input('Enter your login: ')
+# if login == 'admin': print('hi, administrator')
+# else: print('hi, ' + login)
+
+# num = int(input('give the number: '))
+# if num > 0: print('number is positive')
+# elif num == 0: print('number is not positive and not negative')
+# else: print('number is negative')
+
+# num = int(input('Enter number: '))
+# if num > 0: print(num - 10)
+# elif num < 0: print(num + 10)
+# else: print(num +100)
+
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+# num3 = int(input('Enter 3rd number: '))
+# num4 = int(input('Enter 4th number: '))
+# pCounter = 0
+# nCounter = 0
+# if num1 > 0: pCounter += 1
+# else: nCounter +=1
+# if num2 > 0: pCounter += 1
+# else: nCounter +=1
+# if num3 > 0: pCounter += 1
+# else: nCounter +=1
+# if num4 > 0: pCounter += 1
+# else: nCounter +=1
+# print(pCounter, nCounter)
+
+# num = int(input('Enter 1st number: '))
+# if num % 2 == 0: print('even number')
+# else: print('number is odd')
---
+++ practice/practice.py
@@ -0,0 +1,14 @@
+# num = (int(input('give the number: ')))
+# if num % 2 == 0: print('number is even')
+# else: print('number is odd')
+
+num = (int(input('give the number: ')))
+if num > 0: print('positive number')
+elif num == 0: print('not positive, not negative')
+else: print('negative number')
+
+# num1 = (int(input('give the 1st number: ')))
+# num2 = (int(input('give the 2nd number: ')))
+# if num1 > num2: print (num1)
+# elif num1 == num2: print('they are the same') 
+# else: print (num2)
